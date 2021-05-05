c757df4a73d982066ec6da912b8144680ec30989
Shriram Srinivasan <shrirams@lanl.gov>
2021-05-05 16:34:51 -0600

    network transient case now working

--- src/8paper_VG_AZ.jl
+++ src/8paper_VG_AZ.jl
@@ -31,13 +31,17 @@ ts = initialize_simulator(folder)
 
 run_simulator!(ts)
 
+
+
+
 cmp1 = ts.sol["compressors"]["1"]["compression_ratio"]
 cmp2 = ts.sol["compressors"]["2"]["compression_ratio"]
 cmp3 = ts.sol["compressors"]["3"]["compression_ratio"]
 
 pr_node5 = ts.sol["nodes"]["5"]["pressure"]
 
-inflow_node1 = ts.sol["pipes"]["1"]["in_flow"]
+inflow_node6 = ts.sol["pipes"]["1"]["in_flow"]
+outflow_node2 = ts.sol["pipes"]["1"]["out_flow"]
 
 outflow_node5 = ts.sol["pipes"]["5"]["out_flow"]
 outflow_node3 =  ts.sol["pipes"]["2"]["out_flow"] .- ts.sol["pipes"]["3"]["in_flow"]
@@ -47,20 +51,21 @@ t = ts.sol["time_points"]/3600 #hrs
 
 fig, ax = PyPlot.subplots(4, 1, figsize=(6, 12), sharex=true)
 ax[2, 1].plot(t, pr_node5/1e6)
-ax[1, 1].plot(t, inflow_node1)
-ax[3, 1].plot(t, outflow_node5)
-ax[3, 1].plot(t, outflow_node3)
-ax[4, 1].plot(t, cmp1)
-ax[4, 1].plot(t, cmp2)
-ax[4, 1].plot(t, cmp3)
+
+ax[1, 1].plot(t, inflow_node6, t, outflow_node2)
+ax[1, 1].legend(["pipe 1 inflow at n6", "pipe1 outflow at n2"])
+
+ax[3, 1].plot(t, outflow_node5, t, outflow_node3)
+ax[3, 1].legend(["node 5", "node 3"])
+
+ax[4, 1].plot(t, cmp1, t, cmp2, t, cmp3)
 ax[4, 1].legend(["comp 1", "comp 2", "comp 3"])
 
 
 ax[2, 1].set_xlabel("time (hrs)")
-
 ax[2, 1].set_ylabel("pressure at node 5 (MPa)")
-ax[1, 1].set_ylabel("mass inflow at node 1 (kg/s)")
-ax[3, 1].set_ylabel("withdrawal at node 5 and 3 (kg/s)")
+ax[1, 1].set_ylabel("mass flow  (kg/s)")
+ax[3, 1].set_ylabel("withdrawal (kg/s)")
 ax[4, 1].set_ylabel("Compressor Ratios")
 
 fig.tight_layout()
