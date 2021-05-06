d4940bf9fb4f73f28e30706cad8ff827ce76814e
Shriram Srinivasan <shrirams@lanl.gov>
2021-05-06 00:54:24 -0600

    fixed vectorization

--- src/core/eos.jl
+++ src/core/eos.jl
@@ -20,14 +20,14 @@ function _pressure_to_density_simple_cnga(p, nominal_values::Dict{Symbol,Any}, p
     p0, rho0 = nominal_values[:pressure], nominal_values[:density]
     b1, b2 = 1.00300865, 2.96848838e-8
     RT = params[:R] * params[:temperature] / params[:gas_molar_mass]
-    return (p0 * p .* (b1 + b2 * p0 .* p ) / RT ) / rho0
+    return (p0 * p .* (b1 .+ b2 * p0 * p ) / RT ) / rho0
 end
 
 function _density_to_pressure_simple_cnga(rho, nominal_values::Dict{Symbol,Any}, params::Dict{Symbol,Any})
     p0, rho0 = nominal_values[:pressure], nominal_values[:density]
     b1, b2 = 1.00300865, 2.96848838e-8
     RT = params[:R] * params[:temperature] / params[:gas_molar_mass]
-    return (-b1 .+ sqrt.(b1*b1 .+ 4 * b2 * RT * rho0 .* rho) ) / (2.0 * b2) / p0
+    return (-b1 .+ sqrt.(b1 * b1 .+ 4 * b2 * RT * rho0 * rho) ) / (2.0 * b2) / p0
 end
 
 function _pressure_to_density_full_cnga(p, nominal_values::Dict{Symbol,Any}, params::Dict{Symbol,Any})
@@ -37,7 +37,7 @@ function _pressure_to_density_full_cnga(p, nominal_values::Dict{Symbol,Any}, par
     b1 = 1.0 + a1 * (p_atm/6894.75729) * ( 10 ^ (a2 * G) ) / (1.8 * T) ^ a3
     b2 = a1 * (10.0 ^ (a2 * G) ) / (6894.75729 * (1.8 * T)^a3 )
     Rg = params[:R]  / params[:gas_molar_mass]
-    return (p0 * p .* (b1 + b2 * p0 .* p ) / (Rg * T) ) / rho0
+    return (p0 * p .* (b1 .+ b2 * p0 * p ) / (Rg * T) ) / rho0
 end
 
 function _density_to_pressure_full_cnga(rho, nominal_values::Dict{Symbol,Any}, params::Dict{Symbol,Any})
@@ -47,5 +47,5 @@ function _density_to_pressure_full_cnga(rho, nominal_values::Dict{Symbol,Any}, p
     b1 = 1 + a1 * (p_atm/6894.75729) * ( 10 ^ (a2 * G) ) / (1.8 * T) ^ a3
     b2 = a1 * (10 ^ (a2 * G) ) / (6894.75729 * (1.8 * T)^a3 )
     Rg = params[:R]  / params[:gas_molar_mass]
-    return (-b1 .+ sqrt.(b1*b1 .+ 4 * b2 * Rg * T * rho0 .* rho) ) / (2 * b2) / p0
+    return (-b1 .+ sqrt.(b1 * b1 .+ 4 * b2 * Rg * T * rho0 * rho) ) / (2 * b2) / p0
 end
