ba57a2e293c06c561900f0f3d2dbe9d87e1bd0a4
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2024-06-28 15:25:41 -0600

    Update run_ts.jl
    Adds new progress meter syntax

--- src/core/run_ts.jl
+++ src/core/run_ts.jl
@@ -6,7 +6,7 @@ function run_simulator!(ts::TransientSimulator;
     t_0 = params(ts, :t_0)
     num_steps = Int(round((t_f-t_0)/dt))
     output_data = OutputData(ts)
-    @showprogress "Simulation progress: " for _ in 1:num_steps
+    @showprogress desc="Simulation progress: " for _ in 1:num_steps
     	advance_current_time!(ts, dt)
     	#  if current_time is where some disruption occurs, modify ts.ref now
     	advance_pipe_density_internal!(ts, run_type) # (n+1) level
